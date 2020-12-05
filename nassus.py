c3dda61c4a399a82d7da0708da9e71dac358a7e0
jonholdship <jonholdship@gmail.com>
2020-12-05 15:15:03 +0000

    Store contents persist between page loads

--- nassus.py
+++ nassus.py
@@ -6,8 +6,12 @@ app= Flask(__name__)
 def hello():
 	return render_template("index.html")
 
-@app.route("/general-store")
-def general():
-	store=Store("general_store.csv")
+@app.route("/general-store/", defaults={'level': None})
+@app.route("/general-store/<level>")
+def generate_general(level):
+	print(level)
+	store=Store("general_store")
+	if level is not None:
+		store.generate_items_list(level)
 	tables,titles=store.get_item_tables()
-	return render_template("store.html",tables=[table.to_html(classes="general-table",index=False) for table in tables],titles=titles)
+	return render_template("store.html",name="General Store",tables=[table.to_html(classes="general-table",index=False) for table in tables],titles=titles,levels=["Level 1","Level 2"])
--- store.py
+++ store.py
@@ -1,7 +1,15 @@
 import pandas as pd
 from os.path import join
 import numpy as np
-data_folder="static/item-data"
+data_folder="static/data"
+
+rule_files={
+	"general_store":join(data_folder,"general_store_rules.csv")
+}
+
+item_files={
+	"general_store":join(data_folder,"general_store_items.csv")
+}
 
 
 class Store:
@@ -10,11 +18,12 @@ class Store:
 	Probability of an item appearing in the store and mark ups are stored in a csv file which is loaded
 	Beyond that, the logic is generic so we should manage with a single store class
 	'''
-	def __init__(self,store_file,level=1):
+	def __init__(self,store_type,level=1):
 		'''
 		initialize a specific store by providing the store specific probability file and upgrade level
 		'''
-		self.store_file=store_file
+		self.rule_file=rule_files[store_type]
+		self.item_file=item_files[store_type]
 		self.current_level=level
 		
 	def get_item_tables(self):
@@ -24,22 +33,23 @@ class Store:
 		titles - names of tables
 		The content of these tables is set by self.get_items()
 		'''
-		table=self.get_items()
+		table=pd.read_csv(self.item_file)
 		table["Title"]=table["Type"]+" ("+table["Subtype"]+")"
 		titles=table["Title"].unique()
 		table=[table[table["Title"]==x] for x in titles]
+		print(titles)
 		return table,titles
 
-	def get_items(self):
+	def generate_items_list(self,level):
 		'''
 		Function that uses the information in the store specific file to filter the item lists
 		Calls a random number generator to determine whether items are in stock
 		Applies mark up
 		'''
 		#load items and filter table 
-		store_df=pd.read_csv(join(data_folder,self.store_file))
-		store_df=store_df[store_df["Level"]==self.current_level]
-		items_df=pd.read_csv(join(data_folder,"mundane.csv")).merge(store_df,on=["Type","Subtype"])
+		rule_df=pd.read_csv(self.rule_file)
+		rule_df=rule_df[rule_df["Level"]==self.current_level]
+		items_df=pd.read_csv(join(data_folder,"mundane.csv")).merge(rule_df,on=["Type","Subtype"])
 
 		#get a random number for every row (0-1) and keep if lower than probability of item being in stock
 		idx=items_df["probability"].map(lambda x: np.random.uniform()<x)
@@ -51,4 +61,4 @@ class Store:
 		items_df["Price"]=items_df["Value"].astype(float)*items_df["mark_up"].astype(float)
 		items_df["Price"]=items_df["Value"].fillna(999).astype(int).astype(str)+" "+items_df["Unit"]
 		items_df=items_df.drop(["Value","Unit","mark_up"],axis=1)
-		return items_df
+		items_df.to_csv(self.item_file,index=False)
